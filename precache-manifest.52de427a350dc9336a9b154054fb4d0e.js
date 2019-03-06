6a68241701f05de94bdcb74b2a5b8f123efadf37
sharmamayank94 <sharmamayank941@gmail.com>
2019-03-06 23:45:30 +0530

    Updates

---
+++ precache-manifest.52de427a350dc9336a9b154054fb4d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phone-dir/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3aa54ad544210659027f",
+    "url": "/phone-dir/static/js/runtime~main.3aa54ad5.js"
+  },
+  {
+    "revision": "7b55c5fc488dd2e3523d",
+    "url": "/phone-dir/static/js/main.7b55c5fc.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/phone-dir/static/js/2.3791395a.chunk.js"
+  },
+  {
+    "revision": "7b55c5fc488dd2e3523d",
+    "url": "/phone-dir/static/css/main.2817ac8b.chunk.css"
+  },
+  {
+    "revision": "6843165ba70fcdea1a926641b004eaae",
+    "url": "/phone-dir/index.html"
+  }
+];
